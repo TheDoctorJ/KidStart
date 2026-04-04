d396fc3aa6ada475000748f2dcf2cd22ee0181c9
GoodOlLeifP <leifpetersen4579@gmail.com>
2026-04-04 15:59:18 -0700

    Added functionality to profile page.
    -Add Interested categories dropdown
    -Preferences load default values
    -To do: Saving changes

--- app/src/main/java/ca/kidstart/kidstart/ProfileFragment.java
+++ app/src/main/java/ca/kidstart/kidstart/ProfileFragment.java
@@ -5,15 +5,97 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.PopupMenu;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class ProfileFragment extends Fragment {
+
+    private View fragmentView;
+    private MaterialButton addInterestButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        fragmentView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        loadProfilePreferences();
+
+        addInterestButton = fragmentView.findViewById(R.id.add_interest);
+        addInterestButton.setOnClickListener(v -> { showAddInterestMenu(); });
+
+        return fragmentView;
+    }
+
+    /**
+     * Loads the age and distance preference into the page.
+     * To do: Load saved profile preference. Currently this method just sets the default.
+     */
+    private void loadProfilePreferences() {
+        AutoCompleteTextView ageAutoComplete = fragmentView.findViewById(R.id.age_autocomplete);
+        AutoCompleteTextView distanceAutoComplete = fragmentView.findViewById(R.id.distance_autocomplete);
+        // if (profilePreferences is null) => set default
+        ageAutoComplete.setText(getResources().getStringArray(R.array.age_array)[0], false);
+        distanceAutoComplete.setText(getResources().getStringArray(R.array.distance_array)[0], false);
+        //else => load profilePreferences
+    }
+
+    /**
+     * Saves the user's changes when changing their profile preferences.
+     * To do: The actual saving of data.
+     */
+    private void setOnChangedProfilePreferences() {
+        TextInputLayout ageDropdown = fragmentView.findViewById(R.id.age_dropdown);
+        TextInputLayout distanceDropdown = fragmentView.findViewById(R.id.distance_dropdown);
+
+        ageDropdown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Save
+            }
+        });
+
+        distanceDropdown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Save
+            }
+        });
+    }
+
+    /**
+     * Shows the popup menu for the user to add interested categories.
+     */
+    private void showAddInterestMenu() {
+
+        PopupMenu popup = new PopupMenu(getContext(), addInterestButton);
+        popup.getMenuInflater().inflate(R.menu.add_interest_menu, popup.getMenu());
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return tryAddNewInterest();
+            }
+        });
+
+        popup.setOnDismissListener(new PopupMenu.OnDismissListener() {
+            @Override
+            public void onDismiss(PopupMenu menu) {
+            }
+        });
+
+        popup.show();
+    }
+
+    /**
+     *
+     */
+    private boolean tryAddNewInterest() {
+        return false;
     }
 }
