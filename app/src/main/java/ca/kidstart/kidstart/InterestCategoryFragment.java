f9fce116d8d17e134d45299e7bf74d29a01a20e9
GoodOlLeifP <leifpetersen4579@gmail.com>
2026-04-06 20:08:24 -0700

    Complete base functionality for profile page
    -Adding and removing interested categories
    -Settings buttons (don't navigate to a new page, but click listeners are set up)

--- app/src/main/java/ca/kidstart/kidstart/InterestCategoryFragment.java
+++ app/src/main/java/ca/kidstart/kidstart/InterestCategoryFragment.java
@@ -7,13 +7,34 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.android.material.textview.MaterialTextView;
 
 public class InterestCategoryFragment extends Fragment {
+
+    private InterestCategory interestCategory;
+    private ImageView interestIcon, removeButton;
+    private MaterialTextView interestTitle, interestDescription;
+
+    public InterestCategoryFragment(InterestCategory newInterestCategory) {
+        this.interestCategory = newInterestCategory;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_interest_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_interest_category, container, false);
+
+        interestIcon = view.findViewById(R.id.interest_icon);
+        interestTitle = view.findViewById(R.id.interest_title);
+        interestDescription = view.findViewById(R.id.interest_description);
+
+        interestIcon.setImageDrawable(interestCategory.getIcon());
+        interestTitle.setText(interestCategory.getName());
+        interestDescription.setText(interestCategory.getDescription());
+
+        return view;
     }
 }
--- app/src/main/java/ca/kidstart/kidstart/ProfileFragment.java
+++ app/src/main/java/ca/kidstart/kidstart/ProfileFragment.java
@@ -2,13 +2,19 @@ package ca.kidstart.kidstart;
 
 import android.os.Bundle;
 
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 
 import com.google.android.material.button.MaterialButton;
@@ -18,16 +24,56 @@ public class ProfileFragment extends Fragment {
 
     private View fragmentView;
     private MaterialButton addInterestButton;
+    private InterestCategory[] interestCategories;
+    private boolean[] selectedInterests;
+    private LinearLayout accountManagementButton, notificationsButton, helpSupportButton, logoutButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         fragmentView = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        // Load saved stuff
         loadProfilePreferences();
+        loadInterestedCategories();
 
+        // Add interest button
         addInterestButton = fragmentView.findViewById(R.id.add_interest);
         addInterestButton.setOnClickListener(v -> { showAddInterestMenu(); });
+
+        // Settings buttons
+        accountManagementButton = fragmentView.findViewById(R.id.account_management_layout);
+        notificationsButton = fragmentView.findViewById(R.id.notifications_layout);
+        helpSupportButton = fragmentView.findViewById(R.id.help_and_support_layout);
+        logoutButton = fragmentView.findViewById(R.id.logout_layout);
+
+        accountManagementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open account settings
+            }
+        });
+
+        notificationsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open notification settings
+            }
+        });
+
+        helpSupportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open help and support
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logout stuff
+            }
+        });
 
         return fragmentView;
     }
@@ -69,17 +115,59 @@ public class ProfileFragment extends Fragment {
     }
 
     /**
+     * Loads the interested categories into the page.
+     * To do: Load saved categories.
+     */
+    private void loadInterestedCategories() {
+        interestCategories = new InterestCategory[]{
+                new InterestCategory(ResourcesCompat.getDrawable(getResources(), R.drawable.science_interest, getContext().getTheme()),
+                        getString(R.string.science_interest_title), getString(R.string.science_interest_description)),
+                new InterestCategory(ResourcesCompat.getDrawable(getResources(), R.drawable.science_interest, getContext().getTheme()),
+                        getString(R.string.computers_interest_title), getString(R.string.computers_interest_description)),
+                new InterestCategory(ResourcesCompat.getDrawable(getResources(), R.drawable.science_interest, getContext().getTheme()),
+                        getString(R.string.art_interest_title), getString(R.string.art_interest_description)),
+                new InterestCategory(ResourcesCompat.getDrawable(getResources(), R.drawable.science_interest, getContext().getTheme()),
+                        getString(R.string.music_interest_title), getString(R.string.music_interest_description)),
+                new InterestCategory(ResourcesCompat.getDrawable(getResources(), R.drawable.science_interest, getContext().getTheme()),
+                        getString(R.string.sports_interest_title), getString(R.string.sports_interest_description)),
+                new InterestCategory(ResourcesCompat.getDrawable(getResources(), R.drawable.science_interest, getContext().getTheme()),
+                        getString(R.string.reading_interest_title), getString(R.string.reading_interest_description))
+        };
+
+        // if (savedCategories == null) => set default
+        selectedInterests = new boolean[interestCategories.length];
+        for (int i = 0; i < selectedInterests.length; i++) selectedInterests[i] = false;
+        // else => load saved categories
+    }
+
+    /**
      * Shows the popup menu for the user to add interested categories.
      */
     private void showAddInterestMenu() {
 
         PopupMenu popup = new PopupMenu(getContext(), addInterestButton);
-        popup.getMenuInflater().inflate(R.menu.add_interest_menu, popup.getMenu());
+        int[] menuItemIds = new int[interestCategories.length];
+
+        // Create a correspondence between interests and menu item ids.
+        for (int i = 0; i < interestCategories.length; i++) {
+            if (!selectedInterests[i])
+                menuItemIds[i] = popup.getMenu().add(0, i, 0, interestCategories[i].getName()).getItemId();
+            else
+                menuItemIds[i] = -1;
+        }
 
         popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                return tryAddNewInterest();
+                // find corresponding interest from item id.
+                for (int i = 0; i < menuItemIds.length; i++) {
+                    if (menuItemIds[i] == item.getItemId()) {
+                        addNewInterest(interestCategories[i]);
+                        selectedInterests[i] = true;
+                        return true;
+                    }
+                }
+                return false;
             }
         });
 
@@ -93,9 +181,41 @@ public class ProfileFragment extends Fragment {
     }
 
     /**
-     *
+     * Add new interest to the profile.
+     * To do: save added interests
      */
-    private boolean tryAddNewInterest() {
-        return false;
+    private void addNewInterest(InterestCategory interest) {
+        // Add new frame to layout
+        LinearLayout container = fragmentView.findViewById(R.id.interests_container);
+
+        FrameLayout frame = new FrameLayout(getContext());
+        frame.setId(View.generateViewId());
+        frame.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        container.addView(frame);
+
+        // Add fragment to frame
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        InterestCategoryFragment interestCategoryFragment = new InterestCategoryFragment(interest);
+        fragmentTransaction.add(frame.getId(), interestCategoryFragment);
+
+        fragmentTransaction.commitNow();
+
+        // Remove interest button.
+        interestCategoryFragment.getView().findViewById(R.id.remove_interest).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentTransaction.remove(interestCategoryFragment);
+                container.removeView(frame);
+                // Allow interest to be selected again.
+                for (int i = 0; i < interestCategories.length; i++) {
+                    if (interestCategories[i] == interest)
+                        selectedInterests[i] = false;
+                }
+            }
+        });
     }
 }
